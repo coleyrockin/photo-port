233fc0d984ce9a7e278109b966b255c464ec491f
coleyrockin <coleyrockin@aol.com>
2022-10-06 12:37:45 -0400

    test

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -4,7 +4,6 @@ import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Nav from '..';
 
-
 afterEach(cleanup);
 
 describe('Nav component', () => {
